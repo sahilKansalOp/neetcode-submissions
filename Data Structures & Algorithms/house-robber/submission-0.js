c2a56f8100a21e7a7cddea6ea8b3bac8209d9a18
sahilKansalOp <66372251+sahilKansalOp@users.noreply.github.com>
2026-08-12 13:58:34 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        let rob1 = 0,
+            rob2 = 0;
+            for (let i=0;i<nums.length;i++) {
+                const temp = Math.max(rob1+nums[i],rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+            return rob2;
+    }
+}

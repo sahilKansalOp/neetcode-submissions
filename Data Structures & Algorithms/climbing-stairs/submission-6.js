937f865effe7fbe44c01834c2e60d1452fbfdde7
sahilKansalOp <66372251+sahilKansalOp@users.noreply.github.com>
2026-08-18 17:15:17 +0530

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let one = 1,
+            two = 1;
+        for(let i=0;i<n-1;i++) {
+            [one,two] = [one+two,one]
+        }
+        return one;
+    }
+}
